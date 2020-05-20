ea6bcb665f313ca341c0b7cedaea8e7e667453e7
黄雅婷 <huangyt@rosun.com.cn>
2020-05-20 15:06:48 +0800

    initial commit

---
+++ src/test/java/com/tracy/security/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracy.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
